0e9d70b34b554220d83e5db44195a1afbd01f995
Avantika Meharwal <avantikameharwal@gmail.com>
2025-05-05 21:59:31 +0530

    Initial backend code upload

---
+++ api_main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from recommendation_engine import recommend_assessments
+
+app = FastAPI()
+
+# Load dataset once at startup
+catalog_df = pd.read_csv("SHL_Assessment_catalog.csv")
+
+class QueryInput(BaseModel):
+    query: str
+
+@app.post("/recommend")
+def recommend(data: QueryInput):
+    try:
+        recommendations = recommend_assessments(data.query, catalog_df)
+        return recommendations.to_dict(orient="records")
+    except Exception as e:
+        return {"error": str(e)}
